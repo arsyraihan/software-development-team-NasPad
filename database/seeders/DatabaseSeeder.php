d04549e1fb1ace4a1a63d92e5f343d726a571009
wyze.iX <arsyraihanlamavapramudia@gmail.com>
2026-06-13 09:29:44 +0700

    Fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,20 +4,15 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash; // Jangan lupa import ini untuk password
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // Membuat akun khusus untuk PM-Raihan
         User::factory()->create([
             'name' => 'PM-Raihan',
             'email' => 'raihan@gmail.com',
-            'password' => Hash::make('arsyraihan2004'), 
+            'password' => 'arsyraihan2004', 
         ]);
     }
 }
